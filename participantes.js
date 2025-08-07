74fec51e68ef7b5bb611bda0677549624e2a2009
mestredopitaco <suporte@mestredopitaco.com>
2025-08-06 22:05:01 -0300

    Update participantes.js

--- participantes.js
+++ participantes.js
@@ -2,72 +2,73 @@ const participantes = [
   {
     nome: "Benedito (Bene)",
     numeros: [5, 15, 18, 26, 29, 35, 42, 48, 56, 59],
-    inicioConcurso: 2890
+    inicioConcurso: 2900
   },
   {
     nome: "Fabrício Costa",
     numeros: [1, 4, 5, 6, 11, 15, 17, 19, 20, 21],
-    inicioConcurso: 2890
+    inicioConcurso: 2900
   },
   {
     nome: "Wallison Costa",
     numeros: [3, 9, 16, 28, 35, 37, 45, 57, 59, 60],
-    inicioConcurso: 2890
+    inicioConcurso: 2900
   },
   {
     nome: "Lenildo Sousa",
     numeros: [1, 8, 11, 22, 26, 28, 32, 36, 39, 54],
-    inicioConcurso: 2890
+    inicioConcurso: 2900
   },
   {
     nome: "Itallo Teixeira",
     numeros: [2, 8, 13, 19, 24, 31, 35, 44, 51, 57],
-    inicioConcurso: 2890
+    inicioConcurso: 2900
   },
   {
     nome: "Francismar Oliveira",
     numeros: [3, 7, 10, 13, 42, 43, 47, 52, 54, 56],
-    inicioConcurso: 2890
+    inicioConcurso: 2900
   },
   {
     nome: "Marcio Rebouças",
     numeros: [30, 26, 23, 14, 5, 17, 9, 18, 50, 60],
-    inicioConcurso: 2890
+    inicioConcurso: 2900
   },
   {
     nome: "Edelmer Henrique",
     numeros: [2, 6, 23, 8, 56, 58, 45, 38, 59, 17],
-    inicioConcurso: 2890
+    inicioConcurso: 2900
   },
   {
     nome: "Laércio dos Santos",
     numeros: [4, 12, 15, 17, 23, 25, 38, 42, 48, 56],
-    inicioConcurso: 2890
+    inicioConcurso: 2900
   },
   {
     nome: "João Carlos",
     numeros: [2, 8, 18, 37, 43, 48, 54, 56, 27, 32],
-    inicioConcurso: 2890
+    inicioConcurso: 2900
   },
 {
     nome: "Ana Maria Balz",
     numeros: [2, 4, 7, 15, 18, 39, 44, 45, 56, 58],
-    inicioConcurso: 2890
+    inicioConcurso: 2900
   },
   {
     nome: "Cesar Alexandre",
     numeros: [2, 5, 15, 19, 38, 43, 51, 53, 55, 56],
-    inicioConcurso: 2890
+    inicioConcurso: 2900
   },
   {
     nome: "Marilia Felix",
     numeros: [55, 6, 7, 17, 18, 33, 4, 25, 32, 44],
-    inicioConcurso: 2890
+    inicioConcurso: 2900
   },
   {
     nome: "Laércio Sandra Marcos Heliton",
     numeros: [2, 4, 14, 22, 23, 28, 35, 37, 43, 57],
-    inicioConcurso: 2890
+    inicioConcurso: 2900
   }
 ];
+
 
