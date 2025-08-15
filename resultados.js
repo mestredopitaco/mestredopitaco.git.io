39a9aff52c6888d3160bcbc46071ad816407d0e5
mestredopitaco <suporte@mestredopitaco.com>
2025-08-15 08:32:26 -0300

    Update resultados.js

--- resultados.js
+++ resultados.js
@@ -3,8 +3,13 @@ const resultados = [
   {
   concurso: 2900,
    numeros: [33,50,54,55,59,60]
+  },
+  {
+  concurso: 2901,
+   numeros: [02,20,28,38,44,47]
   }
   // Adicione outros concursos aqui
 ];
+
 
 
